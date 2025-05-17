0048c845430a78a60c0fb83d54b3d159049e762e
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-05-17 20:08:06 +0530

    Changes

--- app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,6 @@ import com.example.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LearningJetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -42,6 +40,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LearningJetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Aman")
     }
 }
