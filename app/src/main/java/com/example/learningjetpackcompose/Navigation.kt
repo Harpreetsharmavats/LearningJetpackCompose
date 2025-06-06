9dcc47dffb5e1fd1843bb138abe560c87b77dec3
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-06-06 20:56:34 +0530

    Changes

---
+++ app/src/main/java/com/example/learningjetpackcompose/Navigation.kt
@@ -0,0 +1,84 @@
+package com.example.learningjetpackcompose
+
+import android.text.Layout
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screens.MainScreen.route) {
+        composable(route = Screens.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(route = Screens.DetailScreen.route + "/{name}",
+            arguments = listOf(navArgument("name") {
+            type = NavType.StringType
+            defaultValue = "Android"
+            nullable = true
+        })) {entry->
+DetailsScreen(name = entry.arguments?.getString("name"))
+        }
+    }
+
+
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        TextField(
+            value = text, onValueChange = {
+                text = it
+            }, modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Button(onClick = {
+            navController.navigate(Screens.DetailScreen.withArgs(text))
+
+        }, modifier = Modifier.align(Alignment.End)) {
+            Text("To Details Screen")
+        }
+    }
+
+}
+
+@Composable
+fun DetailsScreen(name: String?) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Hello $name")
+    }
+
+}
+
+
