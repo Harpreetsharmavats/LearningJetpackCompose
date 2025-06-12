f23ad580bd51f9297955355cd21bc694a94b81ef
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-06-12 14:42:18 +0530

    Changes

--- app/src/main/java/com/example/learningjetpackcompose/CalculatorViewmodel.kt
+++ app/src/main/java/com/example/learningjetpackcompose/CalculatorViewmodel.kt
@@ -2,9 +2,45 @@ package com.example.learningjetpackcompose
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class CalculatorViewmodel: ViewModel() {
-    val state by mutableStateOf(CalculatorState())
+    var state by mutableStateOf(CalculatorState())
         private set
+
+    fun onAction(action: CalculatorAction){
+        when(action){
+             is CalculatorAction.Calculate -> performCalculation()
+            is CalculatorAction.Clear -> state = CalculatorState()
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Delete -> performDeletion()
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+        }
+
+    }
+
+    private fun enterOperation(operation: CalculateOperation) {
+        if (state.number1.isNotBlank()){
+            state = state.copy(operation = operation)
+        }
+    }
+
+    private fun enterNumber(i: Int) {
+
+    }
+
+    private fun performDeletion() {
+
+    }
+
+    private fun enterDecimal() {
+
+    }
+
+    private fun performCalculation() {
+
+    }
+
 }
--- app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
+import com.example.learningjetpackcompose.ui.theme.MediumGrey
 
 
 class MainActivity : ComponentActivity() {
@@ -16,6 +21,12 @@ class MainActivity : ComponentActivity() {
                 val viewmodel = viewModel<CalculatorViewmodel>()
                 val state = viewmodel.state
                 val buttonSpacing = 8.dp
+                Calculator(
+                    state = state, onAction = viewmodel::onAction,
+                buttonSpacing = buttonSpacing,
+                    modifier = Modifier.fillMaxSize()
+                        .background(MediumGrey)
+                        .padding(16.dp))
             }
         }
     }
